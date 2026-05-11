a52e740be1443e4b8ed944ebce99ee650a9370dd
szadani12 <daniellyssoares1208@gmail.com>
2026-05-11 15:40:38 -0300

    Tabelas relacionadas categoria e livro

--- library/admin.py
+++ library/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from library.models import Book
+from library.models import Book, Category
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_name',)
+    search_fields = ('category_name',)
+
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'category', 'year', 'price')
-    search_fields = ('category',)
+    search_fields = ('title',)
 
 admin.site.register(Book, BookAdmin) #PARÂMETROS PARA A PÁGINA DE ADMIN
+admin.site.register(Category, CategoryAdmin)
--- library/models.py
+++ library/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    category_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.category_name
+
 class Book(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=70)
     author = models.CharField(max_length=70)
-    category = models.CharField(max_length=70)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='book_category')
     year = models.IntegerField(blank=True, null=True)
     price = models.FloatField(blank=True, null=True)
 
